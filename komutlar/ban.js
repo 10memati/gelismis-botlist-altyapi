5348086efb5b00d5aefa6edebe8693672237ddf9
toprak65 <80392076+toprak65@users.noreply.github.com>
2022-01-28 15:40:21 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -5,10 +5,6 @@ module.exports = {
   $description[
   $username, <@$mentioned[1]>(\`$mentioned[1]\`) Kullanıcısı Banlandı.]
   $color[RANDOM]
-  $sendDm[$mentioned[1];{author:$serverName:$serverIcon}{description:
-  $userTag \`$serverName\` Adlı Sunucudan Yasaklandınız.
-  Sebep: \`\`\`js
-  $noMentionMessage\`\`\`}{color:RANDOM}]
   $ban[$mentioned[1];$noMentionMessage ($userTag Tarafından);7]
   $onlyIf[$noMentionMessage!=;{author:HATA!:$authorAvatar}{description:Yasaklama Sebenini Girin!}{color:RED}{delete:8s}]
   $onlyIf[$mentioned[1]!=;{author:HATA!:$authorAvatar}{description:Yasaklayacağınız Kişiyi Etiketleyin!}{color:RED}{delete:8s}]
