80dbc98d05d14ea8c1ef66ff8c3227dc41f993ac
toprak65 <80392076+toprak65@users.noreply.github.com>
2022-01-28 15:43:22 +0300

    Update forceban.js

--- komutlar/forceban.js
+++ komutlar/forceban.js
@@ -5,12 +5,9 @@ module.exports = {
   $description[
   $username, $userTag[$message[1]](\`$message[1]\`) Kullanıcısı Banlandı.]
   $color[RANDOM]
-  $sendDm[$mentioned[1];{author:$serverName:$serverIcon}{description:
-  $userTag \`$serverName\` Adlı Sunucudan Yasaklandınız.
-  Sebep: \`\`\`js
-  $messageSlice[1]\`\`\`}{color:RANDOM}]
   $ban[$message[1];$messageSlice[1] ($userTag Tarafından);7]
-  $onlyIf[$message[2];{author:HATA!:$authorAvatar}{description:Yasaklama Sebenini Girin!}{color:RED}{delete:8s}]
+  $onlyIf[$message[2]!=;{author:HATA!:$authorAvatar}{description:Yasaklama Sebenini Girin!}{color:RED}{delete:8s}]
+  $onlyIf[$isBanned[$message[1]]!=true;{author:HATA!:$authorAvatar}{description:Bu Kişi Zaten Yasaklı!}{color:RED}{delete:8s}]
   $onlyIf[$message[1]!=;{author:HATA!:$authorAvatar}{description:Yasaklayacağınız Kişinin ID'sini Girin}{color:RED}{delete:8s}]
   $onlyPerms[ban;{author:HATA!:$authorAvatar}{description:Bu Komutu Kullanabilmek İçin \`Üyeleri Yasakla\` Yetkisine Sahip Olmanız Gerekiyor!}{color:RED}{delete:8s}]`
 }
